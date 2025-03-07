deb6914999aaf3aca6e35ed9b63179fb3cd5daf2
Guilherme <gui.carmonunes@gmail.com>
2025-03-07 10:56:39 +0000

    commit teste

---
+++ app/_init_.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(_name_)     
+\
+from app import routes 
+
+
+
+        
---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app impot app
+
+@app.routes('/')
+@app.routes('/indext')
+
+def indext():
+
+    return "Hello, Worlds"
