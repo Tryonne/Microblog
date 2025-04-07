88a8bcdcdbf7ec1a4ad6c8b442657dc60e5a5cbd
Guilherme <gui.carmonunes@gmail.com>
2025-04-07 15:35:36 +0100

    ...

--- app/microblog.py
+++ app/microblog.py
@@ -16,5 +16,5 @@ def artigos():
     return render_template('artigos.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
